b00576d88dde8b71769b558acf917ca9be5fa00e
Yoni Madar <yonim@activefence.com>
2024-03-26 10:57:23 +0200

    debug key mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,13 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>")
+map("n", "<leader>dpr", function()
+  require("dap-python").test_method()
+end)
+map("n", "<leader>dt", ":DapUiToggle<CR>", { noremap = true })
+map("n", "<leader>dc", ":DapContinue<CR>", { noremap = true })
+map("n", "<leader>dr", ":lua require('dapui').open({reset = ture})<CR>", { noremap = true })
+map("n", "<leader>dn", ":lua require('dap-python').test_method()<CR>", { noremap = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
