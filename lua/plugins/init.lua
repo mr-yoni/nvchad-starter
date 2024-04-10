20cc8ab09ec42be4b471ad542cc18fedccb6ba37
Yoni Madar <yonim@activefence.com>
2024-04-10 20:47:17 +0300

    dark notify config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "cormacrelf/dark-notify",
+    config = function()
+      dark = "catppuccin"
+      light = "catppuccin-latte"
+    end
+    lazy = false,
+  },
+  {
     "mfussenegger/nvim-dap",
     config = function()
       require "configs.dap"
