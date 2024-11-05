b7c1b017b409f925191e1e8ef0c5efeddf3742d4
Yoni Madar <yonim@activefence.com>
2024-11-05 10:09:38 +0200

    comment DAP config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -36,12 +36,12 @@ return {
       dap.listeners.before.launch.dapui_config = function()
         dapui.open()
       end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
+      -- dap.listeners.before.event_terminated.dapui_config = function()
+      --   dapui.close()
+      -- end
+      -- dap.listeners.before.event_exited.dapui_config = function()
+      --   dapui.close()
+      -- end
     end,
     event = "VeryLazy",
   },
