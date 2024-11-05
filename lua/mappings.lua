44a17521a759df6327de74d5cff5f3f95adfa4b5
Yoni Madar <yonim@activefence.com>
2024-11-05 10:09:08 +0200

    add gitsigns mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -28,3 +28,73 @@ map("n", "<leader>b", function()
   }
 end, { noremap = true, desc = "Telescope open buffers" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+local lsplinks = require "lsplinks"
+vim.keymap.set("n", "gx", lsplinks.gx, { noremap = true, desc = "LspLinks open document link" })
+
+local gitsigns = require "gitsigns"
+
+-- map("n", "]c", function()
+--   if vim.wo.diff then
+--     vim.cmd.normal { "]c", bang = true }
+--   else
+--     gitsigns.nav_hunk "next"
+--   end
+-- end, { noremap = true, desc = "Gitsigns next hunk" })
+--
+-- map("n", "[c", function()
+--   if vim.wo.diff then
+--     vim.cmd.normal { "[c", bang = true }
+--   else
+--     gitsigns.nav_hunk "prev"
+--   end
+-- end, { noremap = true, desc = "Gitsigns prev hunk" })
+
+-- Navigation
+map("n", "]c", function()
+  if vim.wo.diff then
+    return "]c"
+  else
+    return "<cmd>Gitsigns next_hunk<CR>"
+  end
+end, { noremap = true, desc = "Gitsigns next hunk" })
+map("n", "[c", function()
+  if vim.wo.diff then
+    return "[c"
+  else
+    return "<cmd>Gitsigns prev_hunk<CR>"
+  end
+end, { noremap = true, desc = "Gitsigns prev hunk" })
+-- Actions
+map("n", "<leader>hs", gitsigns.stage_hunk, { noremap = true, desc = "Gitsigns stage hunk" })
+map("n", "<leader>hr", gitsigns.reset_hunk, { noremap = true, desc = "Gitsigns reset hunk" })
+map("n", "<leader>hu", gitsigns.undo_stage_hunk, { noremap = true, desc = "Gitsigns undo stage hunk" })
+map("v", "<leader>hs", function()
+  gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+end, { noremap = true, desc = "Gitsigns stage hunk" })
+map("v", "<leader>hr", function()
+  gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+end, { noremap = true, desc = "Gitsigns reset hunk" })
+map("n", "<leader>hS", gitsigns.stage_buffer, { noremap = true, desc = "Gitsigns stage buffer" })
+map("n", "<leader>hR", gitsigns.reset_buffer, { noremap = true, desc = "Gitsigns reset buffer" })
+map("n", "<leader>hp", gitsigns.preview_hunk, { noremap = true, desc = "Gitsigns preview hunk" })
+map("n", "<leader>hb", function()
+  gitsigns.blame_line { full = true }
+end, { noremap = true, desc = "Gitsigns blame line" })
+map(
+  "n",
+  "<leader>tb",
+  gitsigns.toggle_current_line_blame,
+  { noremap = true, desc = "Gitsigns toggle current line blame" }
+)
+map("n", "<leader>hd", gitsigns.diffthis, { noremap = true, desc = "Gitsigns diff this" })
+map("n", "<leader>hD", function()
+  gitsigns.diffthis "~"
+end, { noremap = true, desc = "Gitsigns diff this ~" })
+map("n", "<leader>td", gitsigns.toggle_deleted, { noremap = true, desc = "Gitsigns toggle deleted" })
+
+-- Disable mappings
+local nomap = vim.keymap.del
+
+nomap("n", "<leader>h")
+nomap("n", "<leader>v")
