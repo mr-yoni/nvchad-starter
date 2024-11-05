6b6bbdb9d7d6669445c1fd5a726cd47eea2a15b5
Yoni Madar <yonim@activefence.com>
2024-11-05 10:06:04 +0200

    set theme

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,9 +1,12 @@
 ---@type ChadrcConfig
 local M = {}
 
+-- prepend path with modern node for running nvim
+vim.cmd 'let $PATH = "/Users/yonimadar/.nvm/versions/node/v20.15.0/bin:" . $PATH'
+
 M.ui = {
 
-  theme_toggle = { "catppuccin", "catppuccin" },
+  theme_toggle = { "catppuccin", "gruvbox_light" },
   theme = "catppuccin",
   -- hl_override = {
   -- 	Comment = { italic = true },
